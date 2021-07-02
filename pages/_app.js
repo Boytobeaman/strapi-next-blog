300885d82f846d0fb93c6e7ad9f2b94081487e82
luxingchao <luxingchao@luxingchaodeMBP.lan>
2021-07-03 07:50:29 +0800

    add product inquiry btn scrolled style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/scss/all.scss'
 import '../components/cat-page-style.scss'
+import '../components/ProductDetailTemplate.scss'
 const App = ({ Component, pageProps }) => <Component {...pageProps} />
 
 export default App
