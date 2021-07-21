357e883dc66355eb00b52623e6ead3ac74adfea9
luxingchao <luxingchao@luxingchaodeMacBook-Pro.local>
2021-07-21 11:51:23 +0800

    add header for local image

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 const withPlugins = require('next-compose-plugins');
 
 
+
 module.exports = {
   trailingSlash: true,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     domains: ['strapi90m.s3.us-west-1.amazonaws.com'],
+  },
+  async headers() {
+    return [
+      {
+        // This doesn't work for 'Cache-Control' key (works for others though):
+        source: '/_next/image(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            // Instead of this value:
+            value: 'public, max-age=259200, s-maxage=259200, stale-while-revalidate=259200',
+            // Cache-Control response header is `public, max-age=60` in production
+            // and `public, max-age=0, must-revalidate` in development
+          },
+        ],
+      },
+    ]
   },
 }
